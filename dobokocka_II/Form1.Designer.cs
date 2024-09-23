5e1d47c3435ce1a66878f4103ef4948eed96ecb7
ricsi <horvath.richard.jozsef_2020c@bolyai-szakkozep.hu>
2024-09-23 13:28:21 +0200

    Add project files.

---
+++ dobokocka_II/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace dobokocka_II
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPlayer1Roll = new System.Windows.Forms.Button();
+            this.buttonPlayer2Roll = new System.Windows.Forms.Button();
+            this.labelPlayer1Roll = new System.Windows.Forms.Label();
+            this.labelPlayer2Roll = new System.Windows.Forms.Label();
+            this.labelRoundWinner = new System.Windows.Forms.Label();
+            this.labelGameResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonPlayer1Roll
+            // 
+            this.buttonPlayer1Roll.Location = new System.Drawing.Point(30, 24);
+            this.buttonPlayer1Roll.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonPlayer1Roll.Name = "buttonPlayer1Roll";
+            this.buttonPlayer1Roll.Size = new System.Drawing.Size(100, 40);
+            this.buttonPlayer1Roll.TabIndex = 0;
+            this.buttonPlayer1Roll.Text = "Player 2 Roll";
+            this.buttonPlayer1Roll.UseVisualStyleBackColor = true;
+            this.buttonPlayer1Roll.Click += new System.EventHandler(this.buttonPlayer1Roll_Click);
+            // 
+            // buttonPlayer2Roll
+            // 
+            this.buttonPlayer2Roll.Location = new System.Drawing.Point(150, 24);
+            this.buttonPlayer2Roll.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonPlayer2Roll.Name = "buttonPlayer2Roll";
+            this.buttonPlayer2Roll.Size = new System.Drawing.Size(100, 40);
+            this.buttonPlayer2Roll.TabIndex = 1;
+            this.buttonPlayer2Roll.Text = "Player 2 Roll";
+            this.buttonPlayer2Roll.UseVisualStyleBackColor = true;
+            this.buttonPlayer2Roll.Click += new System.EventHandler(this.buttonPlayer1Roll_Click);
+            // 
+            // labelPlayer1Roll
+            // 
+            this.labelPlayer1Roll.AutoSize = true;
+            this.labelPlayer1Roll.Location = new System.Drawing.Point(30, 80);
+            this.labelPlayer1Roll.Name = "labelPlayer1Roll";
+            this.labelPlayer1Roll.Size = new System.Drawing.Size(59, 16);
+            this.labelPlayer1Roll.TabIndex = 2;
+            this.labelPlayer1Roll.Text = "Player 1:";
+            // 
+            // labelPlayer2Roll
+            // 
+            this.labelPlayer2Roll.AutoSize = true;
+            this.labelPlayer2Roll.Location = new System.Drawing.Point(150, 80);
+            this.labelPlayer2Roll.Name = "labelPlayer2Roll";
+            this.labelPlayer2Roll.Size = new System.Drawing.Size(59, 16);
+            this.labelPlayer2Roll.TabIndex = 3;
+            this.labelPlayer2Roll.Text = "Player 2:";
+            // 
+            // labelRoundWinner
+            // 
+            this.labelRoundWinner.AutoSize = true;
+            this.labelRoundWinner.Location = new System.Drawing.Point(30, 120);
+            this.labelRoundWinner.Name = "labelRoundWinner";
+            this.labelRoundWinner.Size = new System.Drawing.Size(91, 16);
+            this.labelRoundWinner.TabIndex = 4;
+            this.labelRoundWinner.Text = "Round winner:";
+            // 
+            // labelGameResult
+            // 
+            this.labelGameResult.AutoSize = true;
+            this.labelGameResult.Location = new System.Drawing.Point(30, 160);
+            this.labelGameResult.Name = "labelGameResult";
+            this.labelGameResult.Size = new System.Drawing.Size(92, 16);
+            this.labelGameResult.TabIndex = 5;
+            this.labelGameResult.Text = "Player 1 Wins:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 209);
+            this.Controls.Add(this.labelGameResult);
+            this.Controls.Add(this.labelRoundWinner);
+            this.Controls.Add(this.labelPlayer2Roll);
+            this.Controls.Add(this.labelPlayer1Roll);
+            this.Controls.Add(this.buttonPlayer2Roll);
+            this.Controls.Add(this.buttonPlayer1Roll);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Dice Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPlayer1Roll;
+        private System.Windows.Forms.Button buttonPlayer2Roll;
+        private System.Windows.Forms.Label labelPlayer1Roll;
+        private System.Windows.Forms.Label labelPlayer2Roll;
+        private System.Windows.Forms.Label labelRoundWinner;
+        private System.Windows.Forms.Label labelGameResult;
+    }
+}
---
+++ dobokocka_II/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dobokocka_II
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        private int player1Score = 0;
+        private int player2Score = 0;
+        private int player1Wins = 0;
+        private int player2Wins = 0;
+        private int roundNumber = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPlayer1Roll_Click(object sender, EventArgs e)
+        {
+            PlayRound();
+        }
+
+        private void PlayRound()
+        {
+            // Dobások
+            int player1Roll1 = random.Next(1, 7);
+            int player1Roll2 = random.Next(1, 7);
+            int player2Roll1 = random.Next(1, 7);
+            int player2Roll2 = random.Next(1, 7);
+
+            // Játékosok értékei
+            int player1Result = CalculateDiceResult(player1Roll1, player1Roll2);
+            int player2Result = CalculateDiceResult(player2Roll1, player2Roll2);
+
+            // Kijelző frissítése
+            labelPlayer1Roll.Text = $"Player 1: {player1Roll1}{player1Roll2} ({player1Result})";
+            labelPlayer2Roll.Text = $"Player 2: {player2Roll1}{player2Roll2} ({player2Result})";
+
+            // Kör győztesének eldöntése
+            if (player1Result > player2Result)
+            {
+                player1Wins++;
+                labelRoundWinner.Text = $"Round {roundNumber}: Player 1 wins!";
+            }
+            else if (player2Result > player1Result)
+            {
+                player2Wins++;
+                labelRoundWinner.Text = $"Round {roundNumber}: Player 2 wins!";
+            }
+            else
+            {
+                labelRoundWinner.Text = $"Round {roundNumber}: It's a draw!";
+            }
+
+            roundNumber++;
+
+            // Eredmény kijelzése
+            labelGameResult.Text = $"Player 1 Wins: {player1Wins} | Player 2 Wins: {player2Wins}";
+
+            // Ellenőrzés, hogy van-e győztes
+            if (player1Wins == 6)
+            {
+                MessageBox.Show("Player 1 wins the game!");
+                ResetGame();
+            }
+            else if (player2Wins == 6)
+            {
+                MessageBox.Show("Player 2 wins the game!");
+                ResetGame();
+            }
+            else if (roundNumber > 11)
+            {
+                if (player1Wins > player2Wins)
+                    MessageBox.Show("Player 1 wins the game!");
+                else if (player2Wins > player1Wins)
+                    MessageBox.Show("Player 2 wins the game!");
+                else
+                    MessageBox.Show("It's a tie!");
+                ResetGame();
+            }
+        }
+
+        private int CalculateDiceResult(int die1, int die2)
+        {
+            // A kocka kombinációk erősségi sorrendjének kezelése
+            if (die1 == 2 && die2 == 1) return 100; // Speciális szabály a 21-es dobásra
+            if (die1 == die2) return 90 + die1; // Páros dobások 66-tól 11-ig
+            return die1 * 10 + die2; // Egyéb dobások normál értéke
+        }
+
+        private void ResetGame()
+        {
+            player1Wins = 0;
+            player2Wins = 0;
+            roundNumber = 1;
+            labelPlayer1Roll.Text = "Player 1: ";
+            labelPlayer2Roll.Text = "Player 2: ";
+            labelRoundWinner.Text = "Round winner: ";
+            labelGameResult.Text = "Player 1 Wins: 0 | Player 2 Wins: 0";
+        }
+    }
+}
